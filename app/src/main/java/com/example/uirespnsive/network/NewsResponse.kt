2ef97e3d6ecc6353796282da2501a47e01aa2e8d
abhinay <abhinay212@gmail.com>
2024-12-02 01:45:08 +0530

    Added proguard R8 rules

---
+++ app/src/main/java/com/example/uirespnsive/network/NewsResponse.kt
@@ -0,0 +1,13 @@
+package com.example.uirespnsive.network
+
+import androidx.annotation.Keep
+
+
+/**
+ * Created by Abhinay on 02/12/24.
+ *
+ *
+ */
+ data class NewsResponse (
+    val title: String
+)
